4d1a59a5f19f8b7ff2deb7d7c2ae4c3ac3a539fa
Eduardo <dudusoaressilveira@gmail.com>
2026-08-22 10:42:03 -0300

    Alterarção de pastas da Aula 03

---
+++ Aula03/src/opeadoresaritimeticos/OpeadoresAritimeticos.java
@@ -0,0 +1,85 @@
+
+package opeadoresaritimeticos;
+
+//import java.util.Scanner;
+
+/**
+ *
+ * @author Eduardo Silveira
+ */
+public class OpeadoresAritimeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      /*Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite um valor para A e B, e poderemos ver a media entre A e B:");
+      float a = teclado.nextFloat();
+      float b = teclado.nextFloat();
+      float r = (a + b) / 2;
+      System.out.println("a media entre " + a + " e " + b + " é " +r );
+        
+      int n = 5;
+      n++;
+      System.out.println(n);
+      
+      int nu = 5;
+      int valor = 5 + nu++;
+        System.out.println(valor);
+        System.out.println(nu);
+      
+      int num = 5;
+      int val = 5 + ++num;
+        System.out.println(val);
+        System.out.println(num);
+        
+      int nume = 5;
+      int va = 5 + nume--;
+        System.out.println(va);
+        System.out.println(nume);
+      
+      int numer = 5;
+      int v = 5 + --numer;
+        System.out.println(v);
+        System.out.println(numer);
+      
+      int numero = 5;
+      numero += 2;
+        System.out.println(numero);
+      
+      int numero1 = 5;
+      numero1 -= 2;
+        System.out.println(numero1);
+        
+      int numero2 = 5;
+      numero2 *= 2;
+        System.out.println(numero2);
+      
+      int numero3 = 5;
+      numero3 /= 2;
+        System.out.println(numero3);
+      
+      int numero4 = 5;
+      numero4 %= 2;
+        System.out.println(numero4);
+      
+      float x = 8.9f;
+      int ar = (int)Math.floor(x);
+        System.out.println(ar);
+      
+      float y = 8.2f;
+      int arr = (int)Math.ceil(y);
+        System.out.println(arr);
+      
+      float z = 8.5f;
+      int arre = (int) Math.round(z);
+        System.out.println(arre);*/
+      
+      double ale = Math.random();
+      int d =(int) (0 + ale * (10-0));
+        System.out.println(d);
+    }
+    
+}
